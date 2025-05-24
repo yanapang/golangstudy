483ccb586aaffdbb60162621cfd2a2a02a4d1cb4
yehwang <yanahwang@gmail.com>
2025-05-24 23:46:59 +0900

    init

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	// fmt.Println("Now is : ", time.Now())
+	fmt.Println("Random Number : ", rand.Intn(10))
+}
